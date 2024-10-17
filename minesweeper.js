6f57feb1228ba60096fb18d342430fc938bff43f
Conceptofhumanity <anastasiad.coolgirl@gmail.com>
2024-10-16 23:15:39 -0400

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,37 @@
+const grid = document.querySelector('.grid');
+const size = 5;  
+const mines = 5; 
+const cells = []; 
+
+function createGrid() {
+    for i = 0; i < width * width; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.id = i;
+        cell.addEventListener('click', revealCell);
+        grid.appendChild.(cell)
+        cells.push.cell;
+    }
+    placeMines()
+}
+
+function placeMines() {
+    let mineCount = 0
+    while (mineCount < mines) {
+        const randomIndex = Math.floor(Math.random() * cells.length);
+        if (!Cells[randomIndex].classList.contains('mine')) {
+            cells[randomIndex].classList.add('mine')
+            mineCount++;
+        }
+    }
+}
+
+function revealCell() {
+    if(this.classList.contains('mine')) {
+        alert('BOOOM. haha. nice. hit a bomb.');
+    } else {
+        this.classList.add('revealed');
+    }
+}
+
+createGrid();
